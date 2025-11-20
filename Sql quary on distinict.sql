0292c20d224a22cecb49c990dde703b18251e848
Jay Prakash Gosai <Jayprakashgosai63@gmail.com>
2025-11-20 12:10:10 +0530

    Add files via upload

---
+++ Sql quary on distinict.sql
@@ -0,0 +1,5 @@
+# distinict
+select distinct city from student2;
+select distinct age from student2;
+select distinct age from student2 order by age;
+select distinct city from student2 order by city;
