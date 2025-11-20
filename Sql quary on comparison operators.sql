76d55d041acdd843f3e1236693d1b8fb9c292856
Jay Prakash Gosai <Jayprakashgosai63@gmail.com>
2025-11-20 11:51:41 +0530

    Add files via upload

---
+++ Sql quary on comparison operators.sql
@@ -0,0 +1,8 @@
+# comparison operators
+select*from student2 where age>18;
+select*from student2 where age<18;
+select * from student2 where age<20;
+select * from student2 where age>=18;
+select * from student2 where age<=20;
+select * from student2 where city="agra";
+select * from student2 where city<>"agra";
