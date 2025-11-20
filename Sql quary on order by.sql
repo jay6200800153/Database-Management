11ddea7ad47a148bc07f7ff3f48b8f62f4517f36
Jay Prakash Gosai <Jayprakashgosai63@gmail.com>
2025-11-20 12:07:49 +0530

    Add files via upload

---
+++ Sql quary on order by.sql
@@ -0,0 +1,6 @@
+#order by
+select*from student2 order by name;
+select*from student2 order by name asc;
+select*from student2 order by name desc;
+select*from student2 order by age;
+select*from student2 order by city;
