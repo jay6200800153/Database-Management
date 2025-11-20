b3820edcb1014d9c733cdd9dae3562380585f710
Jay Prakash Gosai <Jayprakashgosai63@gmail.com>
2025-11-20 11:59:17 +0530

    Add files via upload

---
+++ Sql quary on between operator.sql
@@ -0,0 +1,18 @@
+#between operator
+select * from student2 where age between 18 and 20;
+select * from student2 where age not between 18 and 20;
+select * from student2 where id between 2 and 5;
+select * from student2 where id not between 1 and 6;
+select * from student2 where name between "a" and "k";
+
+
+
+
+
+
+
+
+
+
+
+
