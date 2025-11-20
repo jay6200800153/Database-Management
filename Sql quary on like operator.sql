147a45d502fc7deed8ece705aa4ae5715525bc0e
Jay Prakash Gosai <Jayprakashgosai63@gmail.com>
2025-11-20 12:01:23 +0530

    Add files via upload

---
+++ Sql quary on like operator.sql
@@ -0,0 +1,10 @@
+# like operator
+select * from student2 where name like "s%";
+select * from student2 where name like "a%";
+select * from student2 where name like "ram%";
+select * from student2 where name like "rm%";
+select * from student2 where name like "%am%";
+
+
+
+
