883e7f88645a667771b104bfa38079acdb6be030
Jay Prakash Gosai <Jayprakashgosai63@gmail.com>
2025-11-20 12:12:43 +0530

    Add files via upload

---
+++ Sql quary on limit.sql
@@ -0,0 +1,16 @@
+#limit
+select * from student2;
+select * from student2 limit2;
+select * from student2 limit3;
+select * from student2 where city="agra";
+select * from student2 where city="patna"; 
+
+
+
+
+
+
+
+
+
+
