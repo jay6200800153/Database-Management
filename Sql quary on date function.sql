7c0ec2b42d7afc39a85b02e31f0cf62cee9b05d2
Jay Prakash Gosai <Jayprakashgosai63@gmail.com>
2025-11-20 22:03:18 +0530

    Add files via upload

--- Sql quary on date function.sql
+++ Sql quary on date function.sql
@@ -1,6 +1,4 @@
-#data function
-select current_date();
-select curdate();
-select sysdate();
-select date("2025-10-14 10:09:21");
-select week("2025-10-14 10:09:21");
+#time function
+select current_time();
+select curtime();
+select current_timestamp();
