cd1974e6b2207bdac1164a8bfa152a7fad8e8a8c
Jay Prakash Gosai <Jayprakashgosai63@gmail.com>
2025-11-20 21:57:57 +0530

    Add files via upload

---
+++ Sql quary on String function.sql
@@ -0,0 +1,12 @@
+#String function
+select* from student2;
+select concat("raj");
+select concat_ws("_","raj","hello","class");
+select trim("riya");
+select*from student2;
+select position("o" in "helloworld");
+select instr("helloworld","ell");
+select locate("l","helloworld",3);
+select substring("riya",5);
+select reverse("jay prakash");
+
